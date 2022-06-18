081470a675a59896721f49322d6b9d31aeed1db0
TAKAMI1228 <85820427+TAKAMI1228@users.noreply.github.com>
2022-06-18 17:49:03 +0900

    keisan.html,keisan.jsの変更

--- keisan.js
+++ keisan.js
@@ -5,6 +5,10 @@
 丸→四角:(丸型の底面積)÷(スクエア型の底面積)＝X
 */
 
-function cal_kata() {
-
+function cal_Button() {
+    var res_kata = document.getElementById("res_size");
+    var my_kata = document.getElementById("my_size");
+    //丸型
+    var maru = res_kata ^ 2 / (my_kata ^ 2);
+    document.getElementById("result").innerHTML = maru.value;
 }
