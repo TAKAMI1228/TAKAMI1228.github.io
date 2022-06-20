24a796921904bb4758ae39bff2ea002c3a70fb92
TAKAMI1228 <85820427+TAKAMI1228@users.noreply.github.com>
2022-06-20 23:08:23 +0900

    keisan.jsを変更

--- keisan.js
+++ keisan.js
@@ -1,55 +1,46 @@
 /*
+～未実装だけどいずれは実装したいと思っている項目～
 卵の分量は別で作り、1サイズごとに+1
 その他の場合:使いたい型の体積/レシピの型の体積
+もし卵白と卵黄どちらかが余ってしまうレシピの場合はそれを使ったレシピを紹介
+料理の豆知識（Tips）（アクセスするたびにランダムで表示させる）
 */
 
 
 function cal_Button() {
     var res_kata = document.getElementById("res_size");
     var my_kata = document.getElementById("my_size");
-    var result_test = document.getElementById("result1");
-    //console.log(res_kata.value);
-    //console.log(my_kata.value);
 
-    for (var i = 1; i < 2; i++) {
+    for (var i = 1; i < 13; i++) {
         //入力された材料の分量（元レシピの分量）取得
         var mate_g = document.getElementById("zai_g" + String(i));
-        //divの中に要素を挿入用（結果出力）
-        var result_g = document.getElementById("mate" + String(i));
+        //span(id=result~)の中に要素を挿入用（結果出力）
+        var result_test = document.getElementById("result" + String(i));
 
-        //丸型→丸型(使いたい型の半径)^2÷(レシピの型の半径)^2＝X
-        if (document.getElementById("maru_maru").checked) {
-            var maru = (Math.pow((my_kata.value) / 2, 2) / Math.pow((res_kata.value) / 2, 2)).toFixed(1);
-            //document.getElementById("result").textContent = maru;
-
-            //割合を元レシピの材料に掛けて使いたい型の材料の分量を出力
-            var mate_re = mate_g.value * +maru;
-            //result_g.insertAdjacentHTML("beforeend", String(mate_re));
-            console.log(String(mate_re));
+        function mate_Result(kata) {
+            var mate_re = mate_g.value * +kata;
+            result_test.textContent = String(mate_re);
         }
 
-        //四角型→四角型(使いたい型の底面積)÷(レシピの型の底面積)＝X
-        else if (document.getElementById("shikaku_shikaku").checked) {
+        //丸型→丸型　四角型→四角型(使いたい型の底面積)÷(レシピの型の底面積)＝X
+        if (document.getElementById("maru_maru").checked) {
             var shikaku = (Math.pow(my_kata.value, 2) / Math.pow(res_kata.value, 2)).toFixed(1);
-            document.getElementById("result").textContent = shikaku;
+            mate_Result(shikaku);
+            //var mate_re = mate_g.value * +shikaku;
+            //result_test.textContent = String(mate_re);
         }
 
         //丸型→四角型(スクエア型の底面積)÷(丸型の底面積)＝X
         else if (document.getElementById("maru_shikaku").checked) {
             var marushi = (Math.pow(my_kata.value, 2) / (Math.pow(res_kata.value / 2, 2) * 3.14)).toFixed(1);
-            document.getElementById("result").textContent = marushi;
+            mate_Result(marushi);
         }
 
         //四角型→丸型(丸型の底面積)÷(スクエア型の底面積)＝X
         else if (document.getElementById("shikaku_maru").checked) {
             var shimaru = (Math.pow(my_kata.value / 2, 2) * 3.14 / Math.pow(res_kata.value, 2)).toFixed(1);
-            document.getElementById("result").textContent = shimaru;
+            mate_Result(shimaru);
         }
 
-        //選択がない場合
-        else {
-            alert("型の形状を選択してください。");
-        }
     }
-    result_test.textContent = String(mate_re);
 }
