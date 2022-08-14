ab259b5224e09c3098bc27b2a8deaa723ae5fa68
TAKAMI1228 <85820427+TAKAMI1228@users.noreply.github.com>
2022-08-14 19:00:08 +0900

    ヒヨコの非表示修正

--- sample.js
+++ sample.js
@@ -5,48 +5,80 @@ var m=dt.getMonth()+1; //今月
 var mn=dt.getMonth()+2; //来月
 var d=dt.getDate(); //今日の日付
 
+var next14=0;
+var next15=0;
+var dt_next1=0;
+var dt_next2=0;
+var mess="";
+
+//html表示用
+var hiyoday = document.getElementById("hiyoko_day");
+var today=document.getElementById("today");
+var tohiyo=document.getElementById("todayhiyoko");
+var nhiyo=document.getElementById("hiyoko_next");
+var chiyo=document.getElementById("hiyoko_count");
+
 dateT = ["日", "月", "火", "水", "木", "金", "土"];
 var day = dateT[dt.getDay()];//曜日
 
-//来月14日の曜日
-var dt_next1 = new Date(y, mn-1, 14);//月の部分は月-1（javascriptは「実際の月-1」でデータを取る）
-var next14=dateT[dt_next1.getDay()];
-
-//来月15日の曜日
-var dt_next2 = new Date(y, mn-1, 15);
-var next15 = dateT[dt_next2.getDay()];
-
-//あと何日
-var next_piyo=dt_next1.getTime()-dt.getTime();
-var count=Math.floor(next_piyo/(1000*60*60*24));
-//console.log(count);
 
 //改行コード変換
 function Kaigyo(doc){
-  Doc=doc.replace(/\n/,"<br>");
-  document.write(Doc);
+  doc=doc.replace(/\n/,"<br>");
+  document.write(doc);
 }
 
-//時刻カウント
-function t_Real(){
-  //現在時刻
-  var now_h = dt.getHours();//時間
-  var now_m = dt.getMinutes();//分
-  var now_s = dt.getSeconds();//秒
-  var now_real="現在時刻:"+now_h+"時"+now_m+"分"+now_s+"秒";
+//javascriptは「実際の月-1」でデータを取る
+if(d<14){
+  //今月のヒヨコの日を出す。
+  dt_next1 = new Date(y, m-1, 14);
+  next14=dateT[dt_next1.getDay()];
+
+  dt_next2 = new Date(y, m-1, 15);
+  next15 = dateT[dt_next2.getDay()];
+
+  mess="次回のひよこの日: " + y + "年" + m + "月" + 14 + "日 " + next14 + "曜日"+ " & "+m + "月" + 15 + "日 " + next15 + "曜日\n";
 }
 
-//days_k = days.replace(/\n/, "<br>");//改行
+else if(d==14){
+  dt_next1 = new Date(y, mn-1, 14);
+  next14=dateT[dt_next1.getDay()];
 
-Kaigyo("毎月14日、15日は「ひよこの日🐥」\n");
+  var dt_next2 = new Date(y, m-1, 15);
+  next15 = dateT[dt_next2.getDay()];
 
-//今日の日付
-Kaigyo("本日: " + y + "年" + m + "月" + d + "日 " + day + "曜日\n");
+  mess="次回のひよこの日: " + y + "年" + mn + "月" + 14 + "日 " + next14 + "曜日"+ " & "+m + "月" + 15 + "日 " + next15 + "曜日\n";
+}
 
+else if(d>=15){
+  //来月14日の曜日
+  dt_next1 = new Date(y, mn-1, 14);
+  next14=dateT[dt_next1.getDay()];
+
+  //来月15日の曜日
+  dt_next2 = new Date(y, mn-1, 15);
+  next15 = dateT[dt_next2.getDay()];
+
+  mess="次回のひよこの日: " + y + "年" + mn + "月" + 14 + "日 " + next14 + "曜日"+ " & "+mn + "月" + 15 + "日 " + next15 + "曜日\n";
+}
+
+//あと何日
+//14日ver.
+var next_piyo=dt_next1.getTime()-dt.getTime();
+var count=Math.floor(next_piyo/(1000*60*60*24))+1;
+//15日ver.
+var next_piyo2=dt_next2.getTime()-dt.getTime();
+var count2=Math.floor(next_piyo2/(1000*60*60*24))+1;
+
+
+hiyoday.textContent="毎月14日、15日は「ひよこの日🐥」\n";
+today.textContent="本日: " + y + "年" + m + "月" + d + "日 " + day + "曜日\n";
+
+if(d==14 || d==15){
+  tohiyo.textContent="今日はヒヨコの日！！\n";
+}
 
 //次回のひよこの日
-Kaigyo("来月のひよこの日: " + y + "年" + mn + "月" + 14 + "日 " + next14 + "曜日\n");
-Kaigyo("　　　　　　　　 "+ y + "年" + mn + "月" + 15 + "日 " + next15 + "曜日\n");
-
+nhiyo.textContent=mess;
 //次回のひよこの日まであとxx日ってつけたいね
-Kaigyo("次回のひよこの日まであと"+count+"日"+"(14日の場合)と"+(count+1)+"日"+"(15日の場合)");
+chiyo.textContent="次回のひよこの日まであと"+count+"日"+"(14日の場合)と"+count2+"日"+"(15日の場合)";
